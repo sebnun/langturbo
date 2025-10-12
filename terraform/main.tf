ff7c92be671322446d1afac8aa4588d1c4bb6fd4
Pablo Núñez <sebnun@gmail.com>
2025-10-12 19:29:58 +0200

    add terraform infra

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+    }
+  }
+}
+
+provider "oci" {
+  # Configure the OCI provider with your authentication details
+  tenancy_ocid     = local.availability_domain
+  user_ocid        = local.user_ocid
+  fingerprint      = local.fingerprint
+  private_key_path = local.private_api_key_path
+  region           = local.region
+}
+
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = local.availability_domain
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,32 @@
+# The "name" of the availability domain to be used for the compute instance.
+output "name-of-first-availability-domain" {
+  value = data.oci_identity_availability_domains.ads.availability_domains[0]
+}
+# output "objectstorage-namesace" {
+#   value = data.oci_objectstorage_namespace.state_namespace
+# }
+
+#output "vcn_id" {
+#  value = data.oci_core_vcns.main_vcns
+#}
+
+output "vm_list" {
+  value = [
+    for instance in [oci_core_instance.instance1, oci_core_instance.instance2, oci_core_instance.instance3] : {
+      hostname   = instance.display_name
+      ip_address = instance.public_ip
+      user       = "ubuntu"
+    }
+  ]
+}
+
+
+resource "local_file" "ansible_inventory" {
+  filename = "ansible/inventory.ini"
+  content  = <<-EOT
+    [oracl-inst]
+    ${oci_core_instance.instance1.display_name} ansible_host=${oci_core_instance.instance1.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=${local.ssh_pubkey_path}
+    ${oci_core_instance.instance2.display_name} ansible_host=${oci_core_instance.instance2.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=${local.ssh_pubkey_path}
+    ${oci_core_instance.instance3.display_name} ansible_host=${oci_core_instance.instance3.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=${local.ssh_pubkey_path}
+  EOT
+}
