14ffb36a4eabc5430db419ad4ac6b157b81d299c
Pablo Núñez <sebnun@gmail.com>
2025-10-24 12:46:53 +0200

    landing

--- nextjs/app/(site)/page.tsx
+++ nextjs/app/(site)/page.tsx
@@ -2,9 +2,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
-      home
-      <Link href="/es">Learn Spanish</Link>
+    <main className="flex items-center justify-center min-h-screen p-4">
+      <h1 className="text-3xl font-bold">
+        We are relaunching LangTurbo, check back later!
+      </h1>
     </main>
   );
 }
