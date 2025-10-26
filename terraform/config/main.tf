0c395b7da6cba2cf5693eda0e13dcaf0a04020af
Pablo Núñez <sebnun@gmail.com>
2025-10-26 05:36:35 +0100

    update infra

--- terraform/config/main.tf
+++ terraform/config/main.tf
@@ -2,12 +2,3 @@ module "ingress" {
   source = "./modules/nginx-ingress"
   compartment_id = var.compartment_id
 }
-
-# module "certmanager" {
-#   source = "./modules/cert-manager"
-#   letsencrypt_email = var.letsencrypt_email
-# }
-
-# module "longhorn" {
-#   source = "./modules/longhorn"
-# }
