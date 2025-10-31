ea7a36f756bf854a8f1d8de4c63d84faacc31b0a
Pablo Núñez <sebnun@gmail.com>
2025-10-31 18:09:15 +0100

    db in cron

--- cron/jobs/doctor.ts
+++ cron/jobs/doctor.ts
@@ -1,3 +1,8 @@
-export const runDoctorCron =  () => {
+import { db } from "../db/index.ts";
+import { showsTable } from "../db/schema.ts";
+
+export const runDoctorCron = async () => {
+  const shows = await db.select().from(showsTable);
+  console.log('Getting all shows from the database: ', shows)
   console.log("Running doctor cron job...");
 };
