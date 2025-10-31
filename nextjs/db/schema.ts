8111a4b7b806a5042363f5933ab8c3deea51e3e7
Pablo Núñez <sebnun@gmail.com>
2025-10-31 10:42:29 +0100

    shows table

--- nextjs/db/schema.ts
+++ nextjs/db/schema.ts
@@ -1,2 +1,25 @@
-import { pgTable, text, timestamp, boolean } from "drizzle-orm/pg-core";
+import { LANGUAGES_LIST } from "@/lib/languages";
+import { pgTable, text, integer, timestamp, uuid, smallint, pgEnum, boolean } from "drizzle-orm/pg-core";
 
+const isoLanguageCodes = Object.keys(LANGUAGES_LIST);
+
+export const languageCodeEnum = pgEnum("language_code", isoLanguageCodes as [string, ...string[]]);
+export const showTypeEnum = pgEnum("show_type", ["podcast", "youtube", "other"]);
+
+export const showsTable = pgTable("shows", {
+  id: uuid().notNull().primaryKey().defaultRandom(),
+  source_id: text().notNull().unique(), // Should be unique across sources
+  created_at: timestamp().defaultNow().notNull(),
+  title: text().notNull(),
+  description: text(),
+  author: text(),
+  explicit: boolean(),
+  image_url: text().notNull(),
+  show_url: text(),
+  source_url: text(), // Feed url
+  country: text(),
+  category_ids: smallint().array().notNull(),
+  popularity: integer().notNull().default(0),
+  language_code: languageCodeEnum().notNull(),
+  show_type: showTypeEnum().default('podcast').notNull(),
+});
