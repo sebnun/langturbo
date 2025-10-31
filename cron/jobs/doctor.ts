c5aac7b7442b412077f85f01ccd4d92f3cfc4e07
Pablo Núñez <sebnun@gmail.com>
2025-10-31 19:01:10 +0100

    doctor wip

--- cron/jobs/doctor.ts
+++ cron/jobs/doctor.ts
@@ -1,8 +1,35 @@
 import { db } from "../db/index.ts";
 import { showsTable } from "../db/schema.ts";
+import { podcastXmlParser } from "podcast-xml-parser";
+import { USER_AGENT } from "../lib/constants.ts";
+import { eq, sql } from "drizzle-orm";
 
 export const runDoctorCron = async () => {
-  const shows = await db.select().from(showsTable);
-  console.log('Getting all shows from the database: ', shows)
-  console.log("Running doctor cron job...");
+  console.log("Running doctor cron job");
+  const shows = await db.select().from(showsTable).orderBy(showsTable.health_checked_at).limit(1000);
+
+  for (const show of shows) {
+    try {
+      const parsedPodcast = await podcastXmlParser(new URL(show.source_url), {
+        requestHeaders: {
+          "User-Agent": USER_AGENT,
+        },
+      });
+
+      if (parsedPodcast.episodes.length === 0) {
+        //throw new Error("No episodes");
+        // It is possible that they add episodes later?, so don't delete
+        console.log("No episodes found for", show.id);
+      }
+    } catch (e) {
+      console.log("Failed to parse feed, deleting", show.id, e);
+      await db.delete(showsTable).where(eq(showsTable.id, show.id));
+      continue;
+    }
+
+    await db
+      .update(showsTable)
+      .set({ health_checked_at: sql`NOW()` })
+      .where(eq(showsTable.id, show.id));
+  }
 };
