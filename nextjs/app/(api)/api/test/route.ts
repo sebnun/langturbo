1572e975f231a6dca7e61d609a8145821c7f3a04
Pablo Núñez <sebnun@gmail.com>
2025-10-31 08:17:20 +0100

    remove better auth

--- nextjs/app/(api)/api/test/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET(request: Request) {
-  // For example, fetch data from your DB here
-
-  return new Response(JSON.stringify(process.env), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
