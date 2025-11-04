c446f5e10d0d2e14a0be509b846d126198008079
Pablo Núñez <sebnun@gmail.com>
2025-11-04 18:23:22 +0100

    pre setup

--- nextjs/app/api/v1/up/route.ts
+++ nextjs/app/api/v1/up/route.ts
@@ -4,6 +4,7 @@ import { showsTable } from "@/db/schema";
 // Kamal
 
 export async function GET() {
-  await db.select().from(showsTable).limit(1);
+  // TODO
+  //await db.select().from(showsTable).limit(1);
   return new Response("ok");
 }
--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -3,21 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-
-  // https://dlhck.com/thoughts/the-complete-guide-to-self-hosting-nextjs-at-scale
-  async headers() {
-    return [
-      {
-        source: "/:path*{/}?",
-        headers: [
-          {
-            key: "X-Accel-Buffering",
-            value: "no",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
