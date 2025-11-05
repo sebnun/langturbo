135c581cdaaa1cb76272696c6c1c4d7d621a9965
Pablo Núñez <sebnun@gmail.com>
2025-11-05 12:17:35 +0100

    update

--- nextjs/lib/utils.ts
+++ nextjs/lib/utils.ts
@@ -1,3 +1,3 @@
 export const getDatabaseUri = () => {
-  return `postgresql://postgres:${process.env.POSTGRES_PASSWORD!}@localhost:5432/langturbo`;
+  return `postgresql://postgres:${process.env.POSTGRES_PASSWORD!}@127.0.0.1:5432/langturbo`;
 };
