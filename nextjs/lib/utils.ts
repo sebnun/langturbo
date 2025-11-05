7bfc90ba2e25ab350ab0734dff24fbec81da0e0b
Pablo Núñez <sebnun@gmail.com>
2025-11-05 12:28:00 +0100

    fix db connection

--- nextjs/lib/utils.ts
+++ nextjs/lib/utils.ts
@@ -1,3 +1,5 @@
 export const getDatabaseUri = () => {
-  return `postgresql://postgres:${process.env.POSTGRES_PASSWORD!}@127.0.0.1:5432/langturbo`;
+  return `postgresql://postgres:${process.env.POSTGRES_PASSWORD!}@${
+    process.env.NODE_ENV === "development" ? "127.0.0.1" : "nextjs-postgres"
+  }:5432/langturbo`;
 };
