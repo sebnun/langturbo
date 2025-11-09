440de5fb23b6213be5f4ef9d0d1b8fcc2d161717
Pablo Núñez <sebnun@gmail.com>
2025-11-09 17:22:23 +0100

    update

--- nextjs/lib/transcription.ts
+++ nextjs/lib/transcription.ts
@@ -4,11 +4,9 @@ const ai = new GoogleGenAI({
   vertexai: true,
   project: process.env.GOOGLE_CLOUD_PROJECT,
   location: process.env.GOOGLE_CLOUD_LOCATION,
-//   googleAuthOptions: {
-//     clientOptions: {
-//         keyFile : 'gcp-service.json'
-//     }
-//   }
+  googleAuthOptions: {
+      keyFile: "./gcp-service.json",
+  }
 });
 
 export const transcribe = async () => {
