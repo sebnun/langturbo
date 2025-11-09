5133ba25dff0f61be7b1c91a5ef618cf901e97b8
Pablo Núñez <sebnun@gmail.com>
2025-11-09 18:28:36 +0100

    update

--- nextjs/lib/transcription.ts
+++ nextjs/lib/transcription.ts
@@ -1,20 +1,20 @@
 import { GoogleGenAI } from "@google/genai";
 
+const serviceAccount = JSON.parse(Buffer.from(process.env.GOOGLE_KEY_FILE!, "base64").toString("utf-8"));
+
+const ai = new GoogleGenAI({
+  vertexai: true,
+  project: process.env.GOOGLE_CLOUD_PROJECT,
+  location: process.env.GOOGLE_CLOUD_LOCATION,
+  googleAuthOptions: {
+    credentials: serviceAccount,
+  },
+});
+
 export const transcribe = async () => {
-  const serviceAccount = JSON.parse(Buffer.from(process.env.GOOGLE_KEY_FILE!, "base64").toString("utf-8"));
-
-  const ai = new GoogleGenAI({
-    vertexai: true,
-    project: process.env.GOOGLE_CLOUD_PROJECT,
-    location: process.env.GOOGLE_CLOUD_LOCATION,
-    googleAuthOptions: {
-      credentials: serviceAccount,
-    },
-  });
-
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: "why is the sky blue?",
+    contents: "write the name of a color",
   });
   return response.text;
 };
