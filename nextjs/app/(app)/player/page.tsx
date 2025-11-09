4b1bb8a967578dc31fc4a89ae46e342dbd607850
Pablo Núñez <sebnun@gmail.com>
2025-11-09 18:15:20 +0100

    update

--- nextjs/app/(app)/player/page.tsx
+++ nextjs/app/(app)/player/page.tsx
@@ -1,5 +1,7 @@
 import { transcribe } from "@/lib/transcription";
 
+export const dynamic = 'force-dynamic'
+
 export default async function PlayerPage() {
 
   // do check like is this in db already etc, see legacy
--- nextjs/lib/transcription.ts
+++ nextjs/lib/transcription.ts
@@ -4,7 +4,7 @@ import { GoogleGenAI } from "@google/genai";
 
 export const transcribe = async () => {
 
-  const serviceAccount = JSON.parse(process.env.GOOGLE_KEY_FILE || '{}');
+  const serviceAccount = JSON.parse(process.env.GOOGLE_KEY_FILE!);
 
 const ai = new GoogleGenAI({
   vertexai: true,
