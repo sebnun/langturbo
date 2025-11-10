43244d5b354bb40a6242401d7b282192985ff148
Pablo Núñez <sebnun@gmail.com>
2025-11-11 00:28:33 +0100

    cron jpb

--- cron/index.ts
+++ cron/index.ts
@@ -16,7 +16,7 @@ const doctorCron = new CronJob("0 * * * *", runDoctorCron);
 const popularizerCron = new CronJob("0 6 * * *", runPopularizerCron);
 
 // At 18:00 every day
-const scraperCron = new CronJob("0 18 * * *", runScraperCron);
+const scraperCron = new CronJob("30 0 * * *", runScraperCron);
 
 if (jobType === "doctor") {
   doctorCron.start();
