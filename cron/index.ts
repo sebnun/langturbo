04eb7dc1a381d3f5ab9fd90b64e88516f7c9de6e
Pablo Núñez <sebnun@gmail.com>
2025-11-11 02:03:45 +0100

    cron jpb

--- cron/index.ts
+++ cron/index.ts
@@ -1,7 +1,7 @@
 import { runDoctorCron } from "./jobs/doctor.ts";
 import { runPopularizerCron } from "./jobs/popularizer.ts";
 import { runScraperCron } from "./jobs/scraper.ts";
-import { CronJob } from "cron";
+import cron from "node-cron";
 
 if (process.env.NODE_ENV === "development") {
   process.loadEnvFile();
@@ -9,29 +9,13 @@ if (process.env.NODE_ENV === "development") {
 
 const jobType = process.env.JOB_TYPE as "scraper" | "popularizer" | "doctor";
 
-// Every hour
-const d = CronJob.from({
-  cronTime: "0 * * * *",
-  onTick: runDoctorCron,
-  start: jobType === "doctor",
-});
-
-console.log(d.isActive, d.cronTime, "doctor");
-
-// At 06:00 every day
-const p = CronJob.from({
-  cronTime: "0 6 * * *",
-  onTick: runPopularizerCron,
-  start: jobType === "popularizer",
-});
-
-console.log(p.isActive, p.cronTime, "popularizer");
-
-// At 0:55 every day
-const s = CronJob.from({
-  cronTime: "55 0 * * *",
-  onTick: runScraperCron,
-  start: jobType === "scraper",
-});
-
-console.log(s.isActive, s.cronTime, "scraper");
+if (jobType === "scraper") {
+  // At 01:07 every day
+  cron.schedule("07 1 * * *", runScraperCron);
+} else if (jobType === "doctor") {
+  // Every hour
+  cron.schedule("0 * * * *", runDoctorCron);
+} else if (jobType === "popularizer") {
+  // At 06:00 every day
+  cron.schedule("0 6 * * *", runPopularizerCron);
+}
