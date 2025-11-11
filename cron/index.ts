3552730344a607961ee8d744573ffce78bb8f35d
Pablo Núñez <sebnun@gmail.com>
2025-11-11 01:50:08 +0100

    cron jpb

--- cron/index.ts
+++ cron/index.ts
@@ -10,22 +10,28 @@ if (process.env.NODE_ENV === "development") {
 const jobType = process.env.JOB_TYPE as "scraper" | "popularizer" | "doctor";
 
 // Every hour
-CronJob.from({
+const d = CronJob.from({
   cronTime: "0 * * * *",
   onTick: runDoctorCron,
   start: jobType === "doctor",
 });
 
+console.log(d.isActive, d.cronTime, "doctor");
+
 // At 06:00 every day
-CronJob.from({
+const p = CronJob.from({
   cronTime: "0 6 * * *",
   onTick: runPopularizerCron,
   start: jobType === "popularizer",
 });
 
-// At 0:30 every day
-CronJob.from({
-  cronTime: "45 0 * * *",
+console.log(p.isActive, p.cronTime, "popularizer");
+
+// At 0:55 every day
+const s = CronJob.from({
+  cronTime: "55 0 * * *",
   onTick: runScraperCron,
   start: jobType === "scraper",
 });
+
+console.log(s.isActive, s.cronTime, "scraper");
