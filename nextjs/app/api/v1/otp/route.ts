039de3922ea3ffcbcddfe864363a715f2660c2c2
Pablo Núñez <sebnun@gmail.com>
2025-12-05 14:52:09 +0100

    add otp endpoint

---
+++ nextjs/app/api/v1/otp/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@/db";
+import { verification } from "@/db/auth-schema";
+import { desc, eq } from "drizzle-orm";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const email = searchParams.get("email");
+
+  if (email?.toLowerCase() !== process.env.APP_REVIEWER_EMAIL) {
+    return new Response("");
+  }
+
+  const rows = await db
+    .select()
+    .from(verification)
+    .where(eq(verification.identifier, "sign-in-otp-" + process.env.APP_REVIEWER_EMAIL))
+    .orderBy(desc(verification.createdAt))
+    .limit(1);
+
+  return new Response(rows.length ? rows[0].value.replace(":0", "") : "");
+}
